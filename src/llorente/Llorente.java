3fbe5a02b6499b58a23685bb1eb830068b857c9d
Lorente35 <Lorente35@LAPTOP-30B2IGTD>
2018-10-03 16:03:33 +0200

    3 metodes

---
+++ src/llorente/Llorente.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package llorente;
+
+/**
+ *
+ * @author Lorente35
+ */
+public class Llorente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public static int suma() {
+    return 0;    
+    }
+
+    public static int resta() {
+    return 0;    
+    }
+
+    public static int multi() {
+    return 0;
+    }
+    
+}
